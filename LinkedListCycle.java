fa6ac6046937ccf52e09b3d956524296ff19252d
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2022-08-20 08:34:49 +0530

    Update LinkedListCycle.java

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -32,3 +32,36 @@ public class Solution {
 Runtime: 1 ms, faster than 46.08% of Java online submissions for Linked List Cycle.
 Memory Usage: 45.5 MB, less than 73.11% of Java online submissions for Linked List Cycle.
 */
+
+
+//*****************no diffrence but complexity reduced***************
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+       if(head==null) return false;
+       ListNode slow=head;
+       ListNode fast=head;
+        
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Linked List Cycle.
+Memory Usage: 46.7 MB, less than 16.13% of Java online submissions for Linked List Cycle.
+*/
