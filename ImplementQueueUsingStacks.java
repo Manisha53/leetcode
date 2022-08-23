9c671640972344dec4a21eb6e94e02c97b8813e0
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2022-08-23 23:28:49 +0530

    Create ImplementQueueUsingStacks.java

---
+++ ImplementQueueUsingStacks.java
@@ -0,0 +1,126 @@
+/*
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+ 
+
+Example 1:
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+ 
+
+Constraints:
+
+1 <= x <= 9
+At most 100 calls will be made to push, pop, peek, and empty.
+All the calls to pop and peek are valid.
+ 
+
+Follow-up: Can you implement the queue such that each operation is amortized O(1) time complexity? In other words, performing n operations will take overall O(n) time even if one of those operations may take longer
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+ 
+
+Example 1:
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+ 
+
+Constraints:
+
+1 <= x <= 9
+At most 100 calls will be made to push, pop, peek, and empty.
+All the calls to pop and peek are valid.
+ 
+
+Follow-up: Can you implement the queue such that each operation is amortized O(1) time complexity? In other words, performing n operations will take overall O(n) time even if one of those operations may take longer
+*/
+class MyQueue {
+Stack s1=new Stack ();
+Stack s2=new Stack ();
+
+public MyQueue() {
+}
+
+public void push(int x) {
+    while(!s1.isEmpty()) s2.push(s1.pop()); //transfer all the elements from s1 to s2
+    s1.push(x);  //push new element to s1
+    while(!s2.isEmpty()) s1.push(s2.pop()); //transfer all the elements back to s1
+}
+// s1 is ready having last element added to the left most position, hence acting like a queue
+
+public int pop() {
+    if(s1.isEmpty()) return 0;
+   return s1.pop(); 
+}
+
+public int peek() {
+    if(s1.isEmpty()) return 0;     
+    return s1.peek(); 
+}
+
+public boolean empty() {
+    return(s1.isEmpty());
+}
+}
+
+/**
+
+Your MyQueue object will be instantiated and called as such:
+MyQueue obj = new MyQueue();
+obj.push(x);
+int param_2 = obj.pop();
+int param_3 = obj.peek();
+boolean param_4 = obj.empty();
+*/
+
+/*
+
+22 / 22 test cases passed.
+Status: Accepted
+Runtime: 1 ms
+Memory Usage: 41.9 MB
+*/
