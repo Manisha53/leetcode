661f8f7a606491a7aedc092b75ffc33f52481272
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2022-08-27 13:23:46 +0530

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -17,25 +17,38 @@ Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
 
 */
+*/USING FOR LOOP - WAY-1 ====07/14/2022 17:35 Accepted 26 ms 72.6 MB java
 
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        int len=nums.length;
-        Arrays.sort(nums);
-        boolean flag=false;
-        for(int i=0;i<len-1;i++){
-            if(nums[i]==nums[i+1]){
-                flag=true;
-                break;
-            }
-        }
-        return flag;
-    }
+public boolean containsDuplicate(int[] nums) {
+Arrays.sort(nums);
+boolean flag=false;
+for(int i=0;i<nums.length-1;i++){
+if(nums[i]==nums[i+1]) {
+flag=true;
+break;
 }
-/*
-Success
-Details 
-Runtime: 26 ms, faster than 19.90% of Java online submissions for Contains Duplicate.
-Memory Usage: 72.6 MB, less than 9.69% of Java online submissions for Contains Duplicate.
+}
+return flag;
+}
+}
+*/USING HASHSET WAY-2 ====08/27/2022 13:17 Accepted 15 ms 70.5 MB java
 
+class Solution {
+public boolean containsDuplicate(int[] nums) {
+int len=nums.length;
+HashSet hs=new HashSet<>();
+
+    for(int i=0;i<len;i++){
+        if(!hs.contains(nums[i])) hs.add(nums[i]);
+        else return true;
+    }
+    return false;
+}
+}
+
+/*java
+easy-understanding
+for loop array
+hashset java
 */
