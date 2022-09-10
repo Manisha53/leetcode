cef70009c3c1b7543caffd6b06c90f7e8b417dc6
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2022-09-10 23:49:36 +0530

    Update SingleNumber.java

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,3 +1,5 @@
+// BIT MANIPULATION : XOR : x^0=x and x^x=0 : 09/10/2022 23:35	Accepted	9 ms	50.2 MB	java
+
 class Solution {
     public int singleNumber(int[] nums) {
         int result=0;
@@ -5,5 +7,20 @@ class Solution {
             result=result^i;
         }
         return result;
+    }
+}
+// HASHMAP : 09/10/2022 23:47	Accepted	26 ms	53.9 MB	java
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])) map.put(nums[i],2);
+            else map.put(nums[i],1);
+        }
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            if(entry.getValue()==1) return entry.getKey();
+        }
+        return 0;
     }
 }
