53114dd122866244f93d389424e7ede24bf46734
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2022-11-04 11:35:57 +0530

    Update add-two-numbers.java

--- add-two-numbers.java
+++ add-two-numbers.java
@@ -1,3 +1,4 @@
+```
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,17 +11,17 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy=new ListNode(0);
-        ListNode tail=dummy;
-        int carry=0;
+        ListNode dummy=new ListNode(0); //dummy node to act as head for the final-sum linked list
+        ListNode tail=dummy; // to add the single digit to it for adding the elements.
+        int carry=0; // initially it would be 0
         
-        while(l1!=null || l2!=null){
+        while(l1!=null || l2!=null){ // '||' operator is used for the case when either of the 2 lists are exhaused then, the remaining elements of un-exhauseted list can be added to the final sum
             int x=(l1!=null)?l1.val:0;
             int y=(l2!=null)?l2.val:0;
             int sum=x+y+carry;
             carry=sum/10;
             
-            tail.next=new ListNode(sum%10);
+            tail.next=new ListNode(sum%10); //the digit which would be part of the final sum
             tail=tail.next;
             
             if(l1!=null) l1=l1.next;
@@ -28,6 +29,7 @@ class Solution {
         }
         if(carry>0) tail.next=new ListNode(carry); //important step to add carry at msd if it exixts.
         
-        return dummy.next;
+        return dummy.next; // since the first node has a dummy value i.e. 0 ,so dummy.next will give the actual sum.
     }
 }
+```
